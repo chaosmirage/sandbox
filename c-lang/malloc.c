41ddc2fcab9068a68500030512ef95b56f013978
Adel Khamatov <adelkhamatov@yandex.ru>
2020-08-09 20:55:13 +0300

    Fix: add c-lang

---
+++ c-lang/malloc.c
@@ -0,0 +1,21 @@
+// https://www.tutorialspoint.com/c_standard_library/c_function_malloc.htm
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  char *str;
+
+  str = (char *) malloc(15);
+  strcpy(str, "tutorialspoint");
+  printf("String = %s, Address = %u\n", str, str);
+
+  str = (char *) realloc(str, 25);
+  strcat(str, ".com");
+  printf("String = %s, Address = %u\n", str, str);
+
+  free(str);
+
+  return 0;
+}
