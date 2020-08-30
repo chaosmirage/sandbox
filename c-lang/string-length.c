efd54f7dfd8276e8161d38849a558cb92139eff3
Adel Khamatov <adelkhamatov@yandex.ru>
2020-08-30 13:38:23 +0300

    Add string-length example

---
+++ c-lang/string-length.c
@@ -0,0 +1,15 @@
+int string_length(const char *str)
+{
+  // p - адресный тип, char - тип переменной (области памяти ???) расположенной по адресу p
+  const char *p;
+
+  // *p - значение на которое указывает адрес
+  for (p = str; *p; p++) {}
+
+  return p - str;
+}
+
+int main () {
+  printf("%d", string_length("Adel"));
+  return 0;
+}
